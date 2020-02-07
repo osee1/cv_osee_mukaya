4d21bad2ebdf54a76f8eb2fcdb640789b1d9de8d
osee <mukayatshimang@gmail.com>
2020-02-07 12:34:03 +0100

    Ajout de commentaires et corrections des bug javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+/** 
+* ombre sur la photo de profil
+*/
 let photo = document.querySelector('#photo');
 photo.addEventListener('mouseover',(event)=>{
     event.preventDefault();
     photo.classList.toggle('ombre')
 });
-
+/** 
+* ombre sur le bloc de l'entete
+*/ 
 let bloc_div = document.querySelector('#bloc_div');
 bloc_div.addEventListener('mouseover',(event)=>{
     event.preventDefault();
     bloc_div.classList.toggle('bloc_dive');
 });
-
+/**
+ * ombre sur tout les images du bloc contact
+ */
 let photo_contact = document.querySelectorAll('.contact ul li img');
 photo_contact.forEach((photo_contact)=>{
     photo_contact.addEventListener('mouseover',(event)=>{
@@ -18,6 +25,9 @@ photo_contact.forEach((photo_contact)=>{
     });
 });
 
+/**
+ * ombre sur les titres
+ */
 let h2 = document.querySelectorAll('h2');
 const functionTitre = (event) =>{
     event.target.classList.toggle('titre');
@@ -26,6 +36,9 @@ for(let index = 0; index < h2.length; index++){
     h2[index].addEventListener('mouseover', functionTitre);
 }
 
+/**
+ * Ajout et retrait de la couleur blue automatiquement sur tout les titres
+ */
 h2.forEach((titre)=>{
     setInterval(()=>{
         titre.classList.toggle('color');
